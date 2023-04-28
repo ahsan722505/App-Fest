9da2c9b44c7e7dc8c539d37536f8b694fdc9655e
Abubakar <abubakarafzal444@gmail.com>
2023-04-28 13:07:15 +0500

    context added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 import { Poppins, Montserrat } from "next/font/google";
 import Layout from "@/components/Layout";
-
+import { UserProvider } from "@/contexts/userContext";
 const poppins = Montserrat({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -12,9 +12,11 @@ const poppins = Montserrat({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
     </main>
   );
 }
