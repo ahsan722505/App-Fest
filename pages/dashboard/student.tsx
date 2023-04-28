0501ca0a87bda85036c515b63bfa87bea0c994f7
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2023-04-28 14:37:55 +0500

    search Bar Added

--- pages/dashboard/student.tsx
+++ pages/dashboard/student.tsx
@@ -1,3 +1,10 @@
+import Searchbar from "../../components/searchbar";
+
 export default function Student() {
-	return <div>Hello Student</div>;
+	return (
+		<>
+			<div>Hello Student</div>
+			<Searchbar />
+		</>
+	);
 }
