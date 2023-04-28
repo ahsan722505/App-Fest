0313aca3d3ee1c9924625998a8de88866a841961
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2023-04-28 18:32:27 +0500

    Course Enrollment Button

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -11,16 +11,18 @@ const CourseCard = ({ course }: { course: Course }) => {
 	const { user } = useContext(UserContext);
 	return (
 		<div
-			style={{ width: "300px", height: "400px" }}
+			style={{ width: "300px", height: "350px" }}
 			className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 			onClick={() => enrollStatus === "enrolled" || (user.type === "teacher" && router.push(`/courses/${course.id}`))}>
 			<a href="#">
 				<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{course.name}</h5>
 			</a>
 			<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{course.overview}</p>
-			{user.type === "student" && (
-				<RequestEnrollment courseId={course.id} enrollStatus={enrollStatus} setEnrollStatus={setEnrollStatus} />
-			)}
+			<div className="translate-y-40">
+				{user.type === "student" && (
+					<RequestEnrollment courseId={course.id} enrollStatus={enrollStatus} setEnrollStatus={setEnrollStatus} />
+				)}
+			</div>
 		</div>
 	);
 };
